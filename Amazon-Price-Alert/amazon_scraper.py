a0c098444deaedf3385c6fcbf6957655b08d3e23
felipeteixeirabatista007 <felipeteixeirabatista8119@gmail.com>
2025-10-31 15:23:02 -0300

    Update amazon_scraper.py

--- Amazon-Price-Alert/amazon_scraper.py
+++ Amazon-Price-Alert/amazon_scraper.py
@@ -1,4 +1,4 @@
-# Developed and maintained by https://github.com/sarthak1905
+# Developed  and maintained by https://github.com/sarthak1905
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import os
